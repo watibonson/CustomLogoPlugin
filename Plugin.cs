e1e8adafae2091ca01f26ef3c6940ba2aecf17c9
bvolvy <bazilevolvy@gmail.com>
2025-04-30 22:50:25 -0400

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,31 +1,17 @@
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Controller.Plugins;
-using MediaBrowser.Model.Plugins;
 using System;
-using System.Collections.Generic;
 
-namespace Jellyfin.Plugin.CustomLogo
+namespace CustomLogoPlugin
 {
-    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    public class Plugin : BasePlugin<PluginConfiguration>, IPlugin
     {
-        public static Plugin Instance { get; private set; }
-
         public override string Name => "Custom Logo";
-        public override Guid Id => Guid.Parse("2f14d3ec-7f4d-4dfc-b0ce-1a8d7a70d999");
+        public override string Description => "Allows uploading a custom logo to replace Jellyfin branding.";
 
-        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
-            : base(applicationPaths, xmlSerializer)
-        {
-            Instance = this;
-        }
+        public Plugin(IApplicationPaths applicationPaths)
+            : base(applicationPaths) { }
 
-        public IEnumerable<PluginPageInfo> GetPages()
-        {
-            yield return new PluginPageInfo
-            {
-                Name = "CustomLogo",
-                EmbeddedResourcePath = GetType().Namespace + ".Views.configPage.html"
-            };
-        }
+        public override Guid Id => Guid.Parse("abcdef01-2345-6789-abcd-ef0123456789");
     }
 }
